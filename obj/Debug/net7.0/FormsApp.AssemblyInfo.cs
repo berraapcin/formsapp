78165970cc4b1a5d2f0eaf82d7e497a02ca00b5e
berraapcin <berraapcn@gmail.com>
2025-03-25 22:45:00 +0300

    fix

--- obj/Debug/net7.0/FormsApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/FormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3884cba1376b8bd15a67f58fd47e1848c15a91d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
